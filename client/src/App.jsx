0ca8b9863abac99b589225833a42ab2a91a8977d
Shrutika19 <jaiswalshrutika19@gmail.com>
2024-09-05 20:58:01 +0530

    added login file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './Components/Login'
 
 function App() {
 
-
   return (
     <>
-      Slam story
+      <Login />
     </>
   )
 }
