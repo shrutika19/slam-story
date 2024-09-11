9b7bad573bb9cc1e45bcdb1a63b74899c0334cfe
Shrutika19 <jaiswalshrutika19@gmail.com>
2024-09-11 20:45:44 +0530

    added signout functionality

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,27 +1,11 @@
 import React from 'react'
 import loginLeft from '../assets/loginLeft.jpg'
-import googleLogo from '../assets/googlelogo.png';
 import logo from '../assets/logo.png';
-import { useNavigate } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
-
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from '../Shared/firebaseConfig';
 
 function Login() {
-    const navigate = useNavigate();
     console.log("VITE_FIREBASE_APIKEY", import.meta.env.VITE_FIREBASE_APIKEY);
-
-    const handleGoogleAuth = async () => {
-        const provider = new GoogleAuthProvider();
-        try {
-            const result = await signInWithPopup(auth, provider); // Perform Google sign-in
-            console.log("User signed in: ", result.user);
-            navigate('/');
-        } catch (error) {
-            console.error("Error during sign-in:", error);
-        }
-    };
 
     return (
         <div className="flex h-screen">
@@ -29,29 +13,21 @@ function Login() {
                 className="w-full md:w-1/3 bg-cover bg-center p-8 flex items-center justify-center shadow-lg"
                 style={{ backgroundImage: `url(${loginLeft})` }}
             >
-
             </div>
-
             <div className="hidden md:block bg-gray-200" style={{ width: '2px', height: '100%' }}></div>
-
-
             <div className="w-full md:w-2/3 bg-white p-8 flex flex-col items-center justify-center">
                 <div className="w-full max-w-md">
                     <div className="text-5xl font-bold mb-8 text-center" style={{ color: '#0C0C0C' }}>
                         <img src={logo} alt="Logo" />
                     </div>
-
                     <label className="text-lg font-semibold text-left" style={{ color: '#442DC7' }}>
                         Email
                     </label>
-
                     <input
                         type="email"
                         className="w-full p-4 mt-2 mb-6 rounded-full bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-442DC7"
                         placeholder="Enter your email"
                     />
-
-
                     <label className="text-lg font-semibold text-left" style={{ color: '#442DC7' }}>
                         Password
                     </label>
@@ -61,8 +37,6 @@ function Login() {
                         className="w-full p-4 mt-2 mb-2 rounded-full bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-442DC7"
                         placeholder="Enter your password"
                     />
-
-
                     <div className="text-right mb-6">
                         <span className="text-sm font-semibold underline cursor-pointer" style={{ color: '#442DC7' }}>
                             Not registered?
@@ -82,22 +56,7 @@ function Login() {
                         <span className="px-4 text-gray-500">Or, Login With</span>
                         <div className="flex-grow h-px bg-gray-300"></div>
                     </div>
-
-                    <button
-                        className="w-full p-3 rounded-full border border-gray-300 text-black font-semibold text-lg bg-white focus:outline-none focus:ring-2 focus:ring-gray-300 hover:bg-gray-100 transition flex items-center justify-center space-x-2"
-                        onClick={handleGoogleAuth}
-                    >
-                        {/* Google Logo */}
-                        <img
-                            src={googleLogo}
-                            alt="Google logo"
-                            className="w-7 h-7"
-
-                        />
-                        {/* Button Text */}
-                        <span>Sign up with Google</span>
-                    </button>
-
+                    <GoogleAuth />
                 </div>
             </div>
         </div>
