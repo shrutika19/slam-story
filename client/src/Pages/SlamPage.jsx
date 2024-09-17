d528bf0dc31fabf66fdc35285b9bfeb8d89129b3
Shrutika19 <jaiswalshrutika19@gmail.com>
2024-09-17 22:41:13 +0530

    added new component

--- client/src/Pages/SlamPage.jsx
+++ client/src/Pages/SlamPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import SlamHeader from '../Components/SlamHeader'
+import SlamBody from '../Components/SlamBody'
 
 const SlamPage = () => {
     return (
-        <div className="h-screen bg-custom-bg ">
+        // <div className="min-h-screen  bg-custom-bg ">
+        //     <SlamHeader />
+        //     <SlamBody />
+        // </div>
+
+        <div className="min-h-screen bg-custom-bg flex flex-col">
             <SlamHeader />
+            <div className="flex-grow">
+                <SlamBody />
+            </div>
         </div>
     )
 }
