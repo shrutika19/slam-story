5a613d7f5ca254436959d96deffc4ce779518fdb
Shrutika19 <jaiswalshrutika19@gmail.com>
2024-10-07 20:36:17 +0530

    added login api route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,4 +35,33 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser };
+//login user
+// login user
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // Check if user exists
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+
+        // Check if password is correct
+        const isMatch = await bcrypt.compare(password, existingUser.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        // Create and return JWT token
+        const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ token });
+
+    } catch (error) {
+        console.error(error); // Log the actual error
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+
+module.exports = { registerUser, loginUser };
